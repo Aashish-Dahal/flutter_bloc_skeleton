8bfa573587ceafc55cc38f8aa91be13d5706bfd5
Aashish-Dahal <ashishdahal490@gmail.com>
2024-04-29 18:23:37 +0545

    feat: auth repo

--- lib/app/services/auth/interface/auth_interface.dart
+++ lib/app/services/auth/interface/auth_interface.dart
@@ -1,5 +1,11 @@
-import '../../../models/user/index.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fpdart/fpdart.dart';
+
+import '../../../core/error/failure.dart';
+import '../../../core/utils/typedf/index.dart';
 
 abstract class AuthInterface {
-  Future<User> performLogin();
+  Future<Either<Failure, UserCredential>> signUp(JsonMap userMap);
+  Future<Either<Failure, UserCredential>> signIn(JsonMap userMap);
+  Future<Either<Failure, String>> logout();
 }
