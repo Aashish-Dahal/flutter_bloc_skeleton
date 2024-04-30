cf27e097de2fc08539e1e9f413b96c3d693743f4
Aashish-Dahal <ashishdahal490@gmail.com>
2024-04-30 17:50:04 +0545

    feat: getit dependency injection

---
+++ lib/app/injector.dart
@@ -0,0 +1,43 @@
+import 'dart:developer';
+
+import 'package:easy_localization/easy_localization.dart' show EasyLocalization;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'config/api/api.dart';
+import 'core/common/bloc/pagination_bloc.dart';
+import 'core/utils/bloc_observer.dart';
+import 'core/utils/path_provider/index.dart';
+import 'pages/auth/bloc/auth_bloc.dart';
+import 'services/index.dart';
+
+late SharedPreferences preferences;
+final getIt = GetIt.instance;
+
+Future<void> initDependencies() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppPathProvider.initPath();
+  await Firebase.initializeApp();
+  await EasyLocalization.ensureInitialized();
+  EasyLocalization.logger.enableBuildModes = [];
+  InitDio()();
+  preferences = await SharedPreferences.getInstance();
+  Bloc.observer = SkeletonBlocObserver();
+  _initBlocInstance();
+  FlutterError.onError = (error) {
+    log(error.exceptionAsString(), stackTrace: error.stack);
+  };
+}
+
+_initBlocInstance() {
+  getIt.registerLazySingleton(() => AuthBloc(authService));
+  getIt.registerLazySingleton(
+    () => PaginationBloc(newsService.getNews)
+      ..add(
+        const FetchPaginationData(),
+      ),
+  );
+}
