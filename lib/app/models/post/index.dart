856ef29668ae1ac5b728961f178fc5c9146a2523
Aashish-Dahal <ashishdahal490@gmail.com>
2024-05-02 12:25:53 +0545

    feat: post model

---
+++ lib/app/models/post/index.dart
@@ -0,0 +1,47 @@
+import '../../core/utils/typedf/index.dart';
+import '../json_model/index.dart';
+
+List<Posts> _postsFromJson(List data) {
+  return List<Posts>.from(data.map((res) => Posts.fromJson(res)));
+}
+
+class PostM extends JsonModel {
+  PostM({
+    required super.count,
+    required super.data,
+  });
+  factory PostM.fromJson(JsonMap json) => PostM(
+        count: json['total'],
+        data: _postsFromJson(json['posts']),
+      );
+
+  @override
+  JsonMap toJson() => {
+        'data': data,
+        'count': count,
+      };
+}
+
+class Posts {
+  final int id;
+  final String title;
+  final int reaction;
+
+  const Posts({
+    required this.id,
+    required this.title,
+    required this.reaction,
+  });
+
+  factory Posts.fromJson(JsonMap json) => Posts(
+        id: json['id'],
+        title: json['title'],
+        reaction: json['reactions'],
+      );
+
+  JsonMap toJson() => {
+        "id": id,
+        "title": title,
+        "reactions": reaction,
+      };
+}
