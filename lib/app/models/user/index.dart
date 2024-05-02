d81ff03a86094180442cc7173ea4761377bfc4d1
Aashish-Dahal <ashishdahal490@gmail.com>
2024-05-02 12:32:07 +0545

    feat: user model

--- lib/app/models/user/index.dart
+++ lib/app/models/user/index.dart
@@ -1,11 +1,13 @@
+import '../../core/utils/typedf/index.dart';
+
 class User {
   final String email;
   final bool isLoggedIn;
   final String? password;
   User({required this.email, this.password, this.isLoggedIn = false});
 
-  factory User.fromJson(Map<String, dynamic> json) =>
+  factory User.fromJson(JsonMap json) =>
       User(email: json["email"], password: json['password']);
 
-  Map<String, dynamic> toJson() => {"email": email, "password": password};
+  JsonMap toJson() => {"email": email, "password": password};
 }
