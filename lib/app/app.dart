738293d07b9783fa5a44514dc248a5ed6778622a
Aashish-Dahal <ashishdahal490@gmail.com>
2024-05-20 14:08:09 +0545

    feat: responsive design layout

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,9 +2,10 @@ import 'package:easy_localization/easy_localization.dart'
     show BuildContextEasyLocalizationExtension;
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart'
-    show BuildContext, MaterialApp, StatelessWidget, Widget;
+    show BuildContext, MaterialApp, Size, StatelessWidget, Widget;
 import 'package:flutter_bloc/flutter_bloc.dart'
     show BlocProvider, MultiBlocProvider;
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'config/routes/app_routes.dart' show AppRouter;
 import 'config/theme/app_theme.dart' show AppTheme;
@@ -18,14 +19,17 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [BlocProvider.value(value: getIt<AuthBloc>())],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: kDebugMode,
-        routerConfig: AppRouter(getIt<AuthBloc>()).router,
-        title: 'Flutter Bloc Skeleton',
-        theme: AppTheme.light,
-        darkTheme: AppTheme.dark,
-        localizationsDelegates: [...context.localizationDelegates],
-        supportedLocales: context.supportedLocales,
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        builder: (context, child) => MaterialApp.router(
+          debugShowCheckedModeBanner: kDebugMode,
+          routerConfig: AppRouter(getIt<AuthBloc>()).router,
+          title: 'Flutter Bloc Skeleton',
+          theme: AppTheme.light,
+          darkTheme: AppTheme.dark,
+          localizationsDelegates: [...context.localizationDelegates],
+          supportedLocales: context.supportedLocales,
+        ),
       ),
     );
   }
