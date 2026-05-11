344e87ed62cdb8b69b0e4a30e1d513e0a053ee84
Aashish-Dahal <ashishdahal490@gmail.com>
2026-05-11 13:21:50 +0545

    feat: Implement ProfileRemoteDataSource and rename ProductRepository to ProfileRepository

--- lib/features/profile/domain/repository/profile_repository.dart
+++ lib/features/profile/domain/repository/profile_repository.dart
@@ -1,6 +1,6 @@
 import '../../../../core/network/api_result.dart';
 import '../../../auth/domain/entities/user_entity.dart';
 
-abstract class ProductRepository {
+abstract class ProfileRepository {
   Future<ApiResult<UserEntity>> getProfile();
 }
